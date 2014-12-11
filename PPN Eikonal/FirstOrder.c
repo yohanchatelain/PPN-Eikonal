5bd814f29d10640095c6450a69785353691fde26
yohanchatelain <yohan.chatelain@gmail.com>
2014-12-11 12:31:09 +0100

    Add firstorder.c

---
+++ PPN Eikonal/FirstOrder.c
@@ -0,0 +1,9 @@
+//
+//  FirstOrder.c
+//  PPN Eikonal
+//
+//  Created by Yohan on 11/12/2014.
+//  Copyright (c) 2014 Yohan. All rights reserved.
+//
+
+#include "FirstOrder.h"
---
+++ PPN Eikonal/FirstOrder.h
@@ -0,0 +1,14 @@
+//
+//  FirstOrder.h
+//  PPN Eikonal
+//
+//  Created by Yohan on 11/12/2014.
+//  Copyright (c) 2014 Yohan. All rights reserved.
+//
+
+#ifndef __PPN_Eikonal__FirstOrder__
+#define __PPN_Eikonal__FirstOrder__
+
+#include <stdio.h>
+
+#endif /* defined(__PPN_Eikonal__FirstOrder__) */
